7774ffaa9c488250a76e78548a6f78a09ae78f1e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-07-04 15:27:36 -0500

    added a comment

--- LocalNotificationsDemo/LocalNotificationsDemo/LocalNotificationsDemoApp.swift
+++ LocalNotificationsDemo/LocalNotificationsDemo/LocalNotificationsDemoApp.swift
@@ -1,5 +1,8 @@
 import SwiftUI
 
+// This is based on the YouTube videos by Stewart Lynch
+// at https://www.youtube.com/watch?v=tNaSlfLeCB0.
+
 @main
 struct LocalNotificationsDemoApp: App {
     @StateObject var lnManager = LocalNotificationManager()
